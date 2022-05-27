e373734834f32f9b572659ac4b0f77bc02aea1a6
franco dominguez <frann241297@gmail.com>
2022-05-26 22:00:12 -0300

    Se agrego el constructor de la clase User y los getters and setters

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,7 +15,100 @@ public class User {
     private String email;
     private String password;
 
-    private List<Transaction> transactionList;
-    private List<Wallet> walletList;
+    private List<Transaction> transactionList = new ArrayList<>();
+    private List<Wallet> walletList = new ArrayList<>();
 
+    public User(String userName, String name, String lastName, LocalDate dateOfBirth, String email, String password) {
+        this.userName = userName;
+        this.name = name;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getWalletId() {
+        return walletId;
+    }
+
+    public void setWalletId(UUID walletId) {
+        this.walletId = walletId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Transaction> getTransactionList() {
+        return transactionList;
+    }
+
+    public void setTransactionList(List<Transaction> transactionList) {
+        this.transactionList = transactionList;
+    }
+
+    public List<Wallet> getWalletList() {
+        return walletList;
+    }
+
+    public void setWalletList(List<Wallet> walletList) {
+        this.walletList = walletList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                ", userName='" + userName + '\'' +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", email='" + email + '\'' +
+                ", transactionList=" + transactionList +
+                ", walletList=" + walletList +
+                '}';
+    }
 }
