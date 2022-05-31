c857d5eca82b2d0566d7bc4e5146c24d66a57811
Tinchomirandda <tinchomirandda@gmail.com>
2022-05-30 21:14:55 -0300

    Creacion de un usuario WIP.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        User user = new User();
+        user.register();
+        
     }
 }
