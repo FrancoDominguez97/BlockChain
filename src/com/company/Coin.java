b1258faf62af2d7bce02ba2392c4377018d925fe
Tinchomirandda <tinchomirandda@gmail.com>
2022-05-30 21:14:28 -0300

    Cambios en Objeto Coin, nuevas propiedades

--- src/com/company/Coin.java
+++ src/com/company/Coin.java
@@ -4,5 +4,60 @@ public class Coin {
 
     private String coinName;
     private String coinSymbol; //3 ó 4 caracteres que representan la moneda por ej: BTC para bitcoin.
-    //private Icono
+    //private icon
+    private double amount;
+    private String description;
+
+    public Coin() {
+
+    }
+
+    public Coin(String coinName, String coinSymbol, double amount, String description) {
+        this.coinName = coinName;
+        this.coinSymbol = coinSymbol;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public String getCoinName() {
+        return coinName;
+    }
+
+    public void setCoinName(String coinName) {
+        this.coinName = coinName;
+    }
+
+    public String getCoinSymbol() {
+        return coinSymbol;
+    }
+
+    public void setCoinSymbol(String coinSymbol) {
+        this.coinSymbol = coinSymbol;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Coin{" +
+                "coinName='" + coinName + '\'' +
+                ", coinSymbol='" + coinSymbol + '\'' +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
