b06e48cfc44f5910f542729f2eb828a06b689b7a
Tinchomirandda <tinchomirandda@gmail.com>
2022-06-16 23:50:31 -0300

    Creacion de ventana de verMontos de la wallet.

---
+++ src/com/company/Visuals/CheckAmounts.java
@@ -0,0 +1,74 @@
+package com.company.Visuals;
+
+import com.company.JSON.JsonManager;
+import com.company.Usuarios.User;
+import com.company.enums.CoinName;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CheckAmounts implements ActionListener {
+    JFrame frame = new JFrame();
+
+    String[] options = {CoinName.UTNCOIN.name(),CoinName.BITCOIN.name(),CoinName.ETHERIUM.name(),CoinName.LUNA.name()};
+
+    JComboBox<String> jComboBox = new JComboBox<>(options);
+    JButton jButton = new JButton("Buscar");
+    JLabel jLabel = new JLabel();
+    JLabel messageLabel = new JLabel();
+    JButton back = new JButton();
+
+    String userConnected;
+
+    public CheckAmounts(String userID) {
+        this.userConnected = userID;
+        frame.setTitle("TP FINAL LABORATORIO 3");
+        ImageIcon image = new ImageIcon("logo.png");
+        frame.setIconImage(image.getImage());
+
+        ImageIcon backImage = new ImageIcon("flecha.png");
+        back.setIcon(backImage);
+
+        jComboBox.setBounds(80, 50, 140, 20);
+        back.setBounds(0,0, 50,25);
+        messageLabel.setBounds(90, 200, 400,100);
+        jButton.setBounds(100, 100, 90, 20);
+
+        jLabel.setBounds(90, 100, 400, 100);
+
+        frame.add(jComboBox);
+        frame.add(jButton);
+        frame.add(jLabel);
+        frame.add(messageLabel);
+        frame.add(back);
+
+        jButton.addActionListener(this);
+        back.addActionListener(this);
+
+        back.setFocusable(false);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == jButton) {
+            JsonManager admin = new JsonManager();
+            User user = admin.searchUserByIdWallet(JsonManager.JSON_USERS,userConnected);
+            String selected = "Seleccionaste: " + jComboBox.getItemAt(jComboBox.getSelectedIndex());
+            jLabel.setText(selected);
+
+            //crear una tabla donde aparezca cantidad y tipo de moneda
+            messageLabel.setText("Tenes: " + user.getCoin(jComboBox.getItemAt(jComboBox.getSelectedIndex())));
+        }
+        if (e.getSource() == back){
+            frame.dispose();
+            Program program = new Program(userConnected);
+        }
+    }
+}
+
