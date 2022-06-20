6f69fa0ac7cd892c48b303e0199ea96d37126fc0
franco dominguez <frann241297@gmail.com>
2022-06-20 19:41:59 -0300

    Se agrego Ver BlockChain al enum TransactionToDo.

--- src/com/company/enums/TransactionToDo.java
+++ src/com/company/enums/TransactionToDo.java
@@ -3,7 +3,8 @@ package com.company.enums;
 public enum TransactionToDo {
     SELECCIONE(0),
     NUEVA_TRANSFERENCIA(1),
-    HISTORIAL_TRANSFERENCIAS(2);
+    HISTORIAL_TRANSFERENCIAS(2),
+    VER_BLOCKCHAIN(3);
 
     int value;
     TransactionToDo(int value) {
