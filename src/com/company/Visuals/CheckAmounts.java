ac579c2aea78164c3d51b2f6c1459bcaa183f565
franco dominguez <frann241297@gmail.com>
2022-06-20 18:24:25 -0300

    Se agrego la funcionalidad de swarCoin entre las monedas que tiene el usuario.

--- src/com/company/Visuals/CheckAmounts.java
+++ src/com/company/Visuals/CheckAmounts.java
@@ -8,6 +8,8 @@ import com.company.enums.CoinName;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.util.Objects;
 
 public class CheckAmounts implements ActionListener {
     JFrame frame = new JFrame();
@@ -15,12 +17,16 @@ public class CheckAmounts implements ActionListener {
     String[] options = {CoinName.UTNCOIN.name(),CoinName.BITCOIN.name(),CoinName.ETHEREUM.name(),CoinName.LUNA.name()};
 
     JComboBox<String> jComboBox = new JComboBox<>(options);
-    JButton jButton = new JButton("Buscar");
+    JComboBox<String> swapComboBox = new JComboBox<>(options);
+    JButton search = new JButton("Buscar");
     JLabel jLabel = new JLabel();
     JLabel messageLabel = new JLabel();
     JLabel messageUSD = new JLabel();
     JButton back = new JButton();
     JButton swap = new JButton("Transformar");
+    JButton confirmSwap = new JButton("Confirmar");
+    JLabel swapText = new JLabel();
+    JTextField swapBox = new JTextField();
     String userConnected;
 
     public CheckAmounts(String userID) {
@@ -32,30 +38,49 @@ public class CheckAmounts implements ActionListener {
         ImageIcon backImage = new ImageIcon("flecha.png");
         back.setIcon(backImage);
 
+
         jComboBox.setBounds(80, 50, 140, 20);
+        swapComboBox.setBounds(100, 320, 140, 20);
         back.setBounds(0,0, 50,25);
         messageLabel.setBounds(90, 125, 400,100);
         messageUSD.setBounds(90, 150, 400,100);
-        jButton.setBounds(100, 100, 90, 20);
+        search.setBounds(100, 100, 90, 20);
+        confirmSwap.setBounds(100,350,90,20);
+
+        swapText.setBounds(100,260,250,20);
+        swapText.setText("Ingrese valor a cambiar");
+        swapBox.setBounds(100,290,130,20);
 
         jLabel.setBounds(90, 100, 400, 100);
 
         frame.add(swap);
         frame.add(jComboBox);
-        frame.add(jButton);
+        frame.add(search);
         frame.add(jLabel);
         frame.add(messageLabel);
         frame.add(messageUSD);
         frame.add(back);
+        frame.add(swapBox);
+        frame.add(swapText);
+        frame.add(swapComboBox);
+        frame.add(confirmSwap);
 
         swap.setVisible(false);
         swap.setBounds(100,220,130,30);
         swap.addActionListener(this);
 
-        jButton.addActionListener(this);
+        search.addActionListener(this);
         back.addActionListener(this);
+        confirmSwap.addActionListener(this);
 
         back.setFocusable(false);
+        confirmSwap.setFocusable(false);
+        swap.setFocusable(false);
+
+        confirmSwap.setVisible(false);
+        swapComboBox.setVisible(false);
+        swapBox.setVisible(false);
+        swapText.setVisible(false);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
@@ -65,23 +90,32 @@ public class CheckAmounts implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == jButton) {
-            User user = JsonUser.searchUserByIdWallet(JsonManager.JSON_USERS,userConnected);
+        User user = JsonUser.searchUserByIdWallet(JsonManager.JSON_USERS,userConnected);
+        if (e.getSource() == search) {
             String selected = "Seleccionaste: " + jComboBox.getItemAt(jComboBox.getSelectedIndex());
             jLabel.setText(selected);
             messageLabel.setText("Tenes: " + user.getCoin(jComboBox.getItemAt(jComboBox.getSelectedIndex())));
-            messageUSD.setText("Valor en U$D: " + user.getCoin(jComboBox.getItemAt(jComboBox.getSelectedIndex())).calculateValueInUSD());
+            messageUSD.setText("Valor en U$D: " + new DecimalFormat("#.00000").format(user.getCoin(jComboBox.getItemAt(jComboBox.getSelectedIndex())).calculateValueInUSD()));
             swap.setVisible(true);
         }
         if (e.getSource() == back){
             frame.dispose();
-            User user = JsonUser.searchUserByIdWallet(JsonManager.JSON_USERS,userConnected);
             user.obtenerMenu().show();
         }
         if (e.getSource() == swap) {
-
+            confirmSwap.setVisible(true);
+            swapComboBox.setVisible(true);
+            swapBox.setVisible(true);
+            swapText.setVisible(true);
 
         }
+        if(e.getSource() == confirmSwap){
+            //el codigo del swapeo de monedas.
+            double amount = Double.parseDouble(swapBox.getText());
+            System.out.println(amount);
+            user.swapCoin(jComboBox.getItemAt(jComboBox.getSelectedIndex()),swapComboBox.getItemAt(swapComboBox.getSelectedIndex()),amount);
+        }
+
     }
 }
 
